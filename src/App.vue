47a5d2553050c41fab02a7fac57972ae5d42517b
Mazano Kuhudzai <34074210+makjnr317@users.noreply.github.com>
2021-09-17 23:41:00 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+	<main>
+		<div class="main">
+			<div class="intro">
+				<figure><img src="./assets/images/image-jeremy.png" alt="profile"></figure>
+				<h1>
+					<span>Report for<br></span>Jeremy Robsom
+				</h1>
+			</div>
+			<div class="periods">
+				<p @click="period = 'daily'" :class="{active : period==='daily'}">Daily</p>
+				<p @click="period = 'weekly'" :class="{active : period==='weekly'}">Weekly</p>
+				<p @click="period = 'monthly'" :class="{active : period==='monthly'}">Monthly</p>
+			</div>
+		</div>
+		<Section v-for="stat in stats" :key="stat.title" :data="stat" :period="period"/>
+	</main>
+</template>
+
+<script>
+import Section from "./components/Section.vue"
+
+export default {
+	name: 'App',
+	components: {Section},
+	data(){
+		return{
+			stats: [],
+			period: "weekly"
+		}
+	},
+	mounted(){
+		fetch("/data.json")
+		.then(res => res.json())
+		.then(res => this.stats = res)
+	}
+
+}
+</script>
+
---
+++ src/components/Section.vue
@@ -0,0 +1,20 @@
+<template>
+    <section :id="data.title.replace(' ','')">
+
+        <div class="details">
+            <div class="details-heading">
+                <p class="title">{{data.title}}</p>
+                <figure><img src="../assets/images/icon-ellipsis.svg" alt="ellipsis" class="ellipsis"></figure>
+            </div>
+            <p class="current">{{data.timeframes[period].current}}hrs</p>
+            <p class="previous">{{(this.period === "daily")? "Yesterday" : (this.period === "weekly")? "Last Week" : "Last Month"}} - {{data.timeframes[period].previous}}hrs</p>
+        </div>
+        
+    </section>
+</template>
+
+<script>
+export default {
+    props:["data","period"]
+}
+</script>
